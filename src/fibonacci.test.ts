90d37dafb18934c7c8db89f06c2263954c0fa8b1
Andrej Mokris <mokris.andrej@gmail.com>
2024-09-23 13:43:44 +0200

    add unit test to improve test coverage

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -1,5 +1,12 @@
 import { fibonacci } from "./fibonacci.ts";
 
 test("fibonacci-5", () => {
-  expect(fibonacci(5)).toBe(10);
+  expect(fibonacci(5)).toBe(5);
+});
+
+test("fibonacci-nagative", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
 });
